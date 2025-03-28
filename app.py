b647c17bb88a02f2598cd8f3199c23744430ba99
Medvedina <rgoryanchikov@mail.ru>
2025-03-28 19:03:11 +0300

    feat: Открытие страницы дрона по словарю. Частично готово масштабирование страницы

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ class MainWindow(ctk.CTk):
         self.geometry(f"{self.width}x{self.height}")
         ctk.set_widget_scaling(1 / self.scale_factor)
         ctk.set_window_scaling(1 / self.scale_factor)
+        self.minsize(width=800, height=600)
         self.resizable(width=True, height=True)
 
         self.filter_flag = False
@@ -58,7 +59,7 @@ class MainWindow(ctk.CTk):
         
         self.search_entry = ctk.CTkEntry(self.content_frame, width=1190)
         self.search_entry.grid(row=0, column=0, padx=(10, 0), sticky='n')
-        self.content_frame.configure(width=1340, height=1200)
+        self.content_frame.configure(width=1340, height=900)
         
         self.filter_image = ctk.CTkImage(light_image=Image.open('Images/light_filter.png'))
         self.filter_button = ctk.CTkButton(self.content_frame, text='', width=35, image=self.filter_image, command=self.filter)
@@ -77,38 +78,47 @@ class MainWindow(ctk.CTk):
         self.background_geometry.start()
 
     def dynamic_geometry(self):
-        current_geometry = self.geometry()
-        while self.background_flag:
-            time.sleep(0.01)
-            if current_geometry != self.geometry() or self.content_frame.winfo_width != self.winfo_width()*(1200/1920):
-                if self.filter_flag:
-                    new_width = self.winfo_width()
-                    new_height = self.winfo_height()
+                current_geometry = self.geometry()
+                while self.background_flag:
+                    time.sleep(0.01)
+                    if (current_geometry != self.geometry() 
+                        or self.content_frame.winfo_width() != round(self.winfo_width()*(1340/1920), 0) and not self.filter_flag 
+                        or self.content_frame.winfo_width() != round(self.winfo_width()*(1200/1920), 0) and self.filter_flag):
 
-                    if new_width < 1600:
-                        if self.apply_button.grid_info()['row'] != self.switch.grid_info()['row']+7:
-                            self.apply_button.grid_forget()
-                            self.apply_button.grid(row=self.switch.grid_info()['row']+7, column=0, pady=(10, 0), sticky='nw')
-                    else:
-                        if self.apply_button.grid_info()['row'] == self.switch.grid_info()['row']+7:
-                            self.apply_button.grid_forget()
-                            self.apply_button.grid(row=self.switch.grid_info()['row']+6, column=0, pady=(10, 0), padx=(180, 0), sticky='nw')
+                        print(self.content_frame.winfo_width(), int(self.winfo_width()*(1340/1920)))
+                        if self.filter_flag:
+                            new_width = self.winfo_width()
+                            new_height = self.winfo_height()
 
-                    self.filter_frame.configure(width=new_width*(300/1920), height=new_height*(900/1080))
-                    self.menu_frame.configure(width=new_width*(300/1920), height=new_height*(900/1080))
-                    self.content_frame.configure(width=new_width*(1200/1920), height=new_height*(900/1080))
-                    self.search_entry.configure(width=self.content_frame.winfo_width()-150)
+                            if new_width < 1600:
+                                if self.apply_button.grid_info()['row'] != self.switch.grid_info()['row']+7:
+                                    self.apply_button.grid_forget()
+                                    self.apply_button.grid(row=self.switch.grid_info()['row']+7, column=0, pady=(10, 0), sticky='nw')
+                            elif self.apply_button.grid_info()['row'] == self.switch.grid_info()['row']+7:
+                                    self.apply_button.grid_forget()
+                                    self.apply_button.grid(row=self.switch.grid_info()['row']+6, column=0, pady=(10, 0), padx=(180, 0), sticky='nw')
 
-                    current_geometry = self.geometry()
-                
-                else:
-                    new_width = self.winfo_width()
-                    new_height = self.winfo_height()
-                    self.menu_frame.configure(width=new_width*(300/1920), height=new_height*(900/1080))
-                    self.content_frame.configure(width=new_width*(1340/1920), height=new_height*(900/1080))
-                    self.search_entry.configure(width=self.content_frame.winfo_width()-150)
-                    current_geometry = self.geometry()          
-        return 0
+                            self.filter_frame.configure(width=new_width*(300/1920), height=new_height*(900/1080))
+                            self.menu_frame.configure(width=new_width*(300/1920), height=new_height*(900/1080))
+                            self.content_frame.configure(width=new_width*(1200/1920), height=new_height*(900/1080))
+                            self.search_entry.configure(width=self.content_frame.winfo_width()-150)
+
+                            current_geometry = self.geometry()
+                            continue
+                        else:
+                            new_width = self.winfo_width()
+                            new_height = self.winfo_height()
+                            try:
+                                self.drone_frame.configure(width=new_width*(1200/1920))
+                                for image in self.images_tab.image_list:
+                                    image.configure(size=((int(new_width*(500/1920))), int(new_height*(500/1080))))
+                            except Exception as e:
+                                print(e)
+                            self.menu_frame.configure(width=new_width*(300/1920), height=new_height*(900/1080))
+                            self.content_frame.configure(width=new_width*(1340/1920), height=new_height*(900/1080))
+                            self.search_entry.configure(width=self.content_frame.winfo_width()-150)
+                            current_geometry = self.geometry()
+                            continue       
     
     def switch_event(self, num):
         dictionary_appender = []
@@ -143,6 +153,7 @@ class MainWindow(ctk.CTk):
         self.checkbox_list.clear()
         self.filter_frame.grid_forget()
         self.filter_flag = False
+        self.content_frame.configure(width=1200, height=900)
 
     def filter(self):
         try:
@@ -154,6 +165,7 @@ class MainWindow(ctk.CTk):
         except AttributeError:
             self.filter_frame = ListFrame(self, title='Фильтр')
             self.filter_frame.grid(row=0, column=2, pady=(10, 0), padx=(10, 0), sticky='ne')
+            
             self.switch_list = []
 
             for num in range(6):
@@ -166,8 +178,12 @@ class MainWindow(ctk.CTk):
             
             self.apply_button = ctk.CTkButton(self.filter_frame, text='Применить', command=self.apply_filter, width=100)
             self.apply_button.grid(row=self.switch.grid_info()['row']+6, column=0, pady=(10, 0), padx=(180, 0), sticky='nw')
+            
             self.cancel_button = ctk.CTkButton(self.filter_frame, text='Отмена', command=self.cancel_filter, width=100)
             self.cancel_button.grid(row=self.switch.grid_info()['row']+6, column=0, pady=(10, 0), sticky='nw')
+
+            self.content_frame.configure(width=1340, height=900)
+
             self.filter_flag = True
 
     def deploy_drones(self):
@@ -180,30 +196,36 @@ class MainWindow(ctk.CTk):
             if drone['id'] == id:
                 for widget in self.current_drone:
                     widget.destroy()
+
                 self.drone_frame = ctk.CTkFrame(master=self.content_frame)
-                self.drone_frame.grid_rowconfigure((0,1,2,3), weight=1)
-                self.drone_frame.grid_columnconfigure((0,1,2,3), weight=1)
+                self.drone_frame.grid_rowconfigure((2,3), weight=1)
+                self.drone_frame.grid_columnconfigure(1, weight=1)
                 self.drone_frame.configure(width=1200)
                 self.drone_frame.grid(row=1, column=0, sticky='nsew', columnspan=3)
+                
                 name = drone['name']
                 description = drone['description']
                 type = drone['type']
                 characteristics = drone['characteristics']
                 images = drone['images']
+                
                 self.images_tab = ImagesTab(master=self.drone_frame, images=images)
                 self.drone_descripiton = DroneDescription(master=self.drone_frame, name=name, type=type, characteristics=characteristics, description=description)
                 self.images_tab.grid(row=0, column=0, sticky='w', padx=10, rowspan=self.drone_descripiton.counter+4)
                 self.current_drone = [self.images_tab, self.drone_frame]
+                
                 for widget in self.drone_descripiton.widget_list:
                     self.current_drone.append(widget)
  
 class ImagesTab(ctk.CTkTabview):
     def __init__(self, master, images):
         super().__init__(master)
+        self.image_list = []
         print(master)
         for i in range(len(images)):
             self.add(f'Фото {i+1}')
             self.image = ctk.CTkImage(dark_image=images[i], size=(500, 500))
+            self.image_list.append(self.image)
             self.image_label = ctk.CTkLabel(self.tab(f'Фото {i+1}'), image=self.image, text='')
             self.image_label.pack(side='top')
 
@@ -212,18 +234,23 @@ class DroneDescription:
         print(master)
         self.widget_list = []
         self.counter = 0
+       
         self.name_label = ctk.CTkLabel(master, text=name, font=('Arial', 50, 'bold'))
-        self.name_label.grid(column=1, row=1, sticky='n', padx=20, pady=(10, 0))
+        self.name_label.grid(column=1, row=1, sticky='nw', pady=(10, 0))
+        
         self.widget_list.append(self.name_label)
         self.type_label = ctk.CTkLabel(master, text=type, font=('Arial', 50, 'bold'))
-        self.type_label.grid(column=1, row=2, sticky='n', padx=20, pady=(0, 100))
+        self.type_label.grid(column=1, row=2, sticky='nw', pady=(0, 100))
         self.widget_list.append(self.type_label)
-        self.title_label = ctk.CTkLabel(master, text='Характеристики', font=('Arial', 30, 'bold'))
-        self.title_label.grid(column=1, row=3, sticky='nw', pady=(0, 40))
+
+        self.char_title_label = ctk.CTkLabel(master, text='Характеристики', font=('Arial', 30, 'bold'))
+        self.char_title_label.grid(column=1, row=3, sticky='nw', pady=(10, 0))
+
         for key, value in characteristics.items():
             self.counter += 1
             self.char_label = ctk.CTkLabel(master, text=f'{key}: {value}', font=('Arial', 20, 'bold'))
             self.char_label.grid(column=1, row=3+self.counter, sticky='nw', pady=(0, 0))
+
         self.description_textbox = ctk.CTkTextbox(master, width=1100, font=('Arial', 20, 'bold'), wrap='word')
         self.description_textbox.grid(row=4+self.counter, column=0, sticky='ew', columnspan=2, padx=10, pady=(10, 0))
         self.description_textbox.insert("0.0", description)
@@ -233,10 +260,13 @@ class DroneFrame(ctk.CTkFrame):
     def __init__(self, master, drone, window):
         super().__init__(master)
         self.drone_image = ctk.CTkImage(dark_image=drone['img'], size=(69, 69))
+        
         self.image_label = ctk.CTkLabel(self, image=self.drone_image, text="")
         self.image_label.grid(row=drone['id'], column=0, sticky='nw')
+        
         self.drone_button = ctk.CTkButton(self, text=f'{drone['name']}', command=lambda i=drone['id']:window.open_drone(i))
         self.drone_button.grid(row=drone['id'], column=1, sticky='n')
+        
         self.drone_description = ctk.CTkLabel(self, text=drone['type'])
         self.drone_description.grid(row=drone['id'], column=1, sticky='nw', pady=(30, 0), padx=(5, 0))
         
